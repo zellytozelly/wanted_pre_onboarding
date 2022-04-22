cb89d49032b339e255fa6402a1dab1d8c20eed0b
zellytozelly <zellytozelly@gmail.com>
2022-04-23 02:29:31 +0900

    feat: add slider

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Toggle from "./components/Toggle";
 import Tab from "./components/Tab"
+import Slider from './components/Slider';
 
 function App() {
   return (
     <div className="App">
       <Toggle/>
       <Tab/>
+      <Slider/>
     </div>
   );
 }
---
+++ src/components/Slider.js
@@ -0,0 +1,130 @@
+import React, { useState } from "react";
+import styled from 'styled-components';
+
+export default function Slider(){
+    const [sliderValue, setSliderValue] = useState(0);
+    const splitValues = [1, 25, 50, 75, 100];
+
+
+    return (
+        <div>
+            <SliderWrapper>
+                <TextBox> 
+                    <Text>{sliderValue}</Text> %
+                </TextBox>
+                <MainSlider>
+
+                    <Input
+                        type='range'
+                        style={{ color: `var(--grey-background)` }}
+                        value={sliderValue} 
+                        onChange={(e) => setSliderValue(e.target.value)} 
+                        min='0' 
+                        max='100'
+                        step='1'
+                    />
+                
+                    {splitValues.map((index)=>{
+                        return(
+                            <>
+                                <Circle
+                                    index={index}
+                                    value={sliderValue} 
+                                    
+                                />
+                                <RangeButton
+                                    index={index}
+                                    onClick={(e) => setSliderValue(index)}
+                                >{index}%</RangeButton>
+                            </>
+                        )
+                    })}
+
+                </MainSlider>
+
+            </SliderWrapper>
+        </div>
+    );
+}
+
+
+const SliderWrapper = styled.div`
+    padding: 0 40px 40px 40px;
+    margin: 0 15% 40px 15%;
+    border: 1px solid var(--shadow);
+    border-radius: 5px; 
+    background: var(--white);
+`
+const TextBox = styled.div`
+    padding: 20px;
+    margin: 40px 15%;
+    border: 1px solid var(--shadow);
+    border-radius: 5px; 
+    background: var(--white-second);
+    text-align: right;
+    color: var(--shadow);
+`
+const Text = styled.span`
+    padding: 20px;
+    color: var(--black);
+    font-weight: var(--bold-900);
+    font-size: 1.2em;
+`
+const MainSlider = styled.div`
+    margin: 40px 15%;
+    position: relative;
+    height: 6px;
+    background: var(--grey-background);
+    border-radius: 1px;
+`
+const Input = styled.input`
+    position: absolute;
+    height: 6px;
+    width: 100%;
+    z-index: 1; 
+    background: linear-gradient(90deg, var(--green-mint) 0%, var(--green-mint) 
+        ${(props)=>(props.value)}%, var(--grey-background) ${(props)=>(props.value)}%);
+
+    ::-webkit-slider-thumb{
+        -webkit-appearance: none;
+        background: var(--green-mint);
+        cursor: pointer;
+        width: 25px;
+        height: 25px;
+        border-radius: 50%;
+        border: 4px solid var(--white);
+        box-shadow: 0px 1px 3px gray;   
+    }
+`
+const Circle = styled.div`
+    line-height: 3px;
+    position: absolute;
+    left: calc(${(props)=>(props.index)}%);
+    top: 50%;
+    width: 15px;
+    height: 15px;
+    border-radius: 50%;
+    transform: translate(-50%,-50%);
+    background: ${(props)=>((props.index > props.value) ? "var(--grey-background)" : "var(--green-mint)")};
+`
+const RangeButton = styled.button`
+    position: absolute;
+    left: calc(${(props)=>(props.index)}% - 1.2rem);
+    margin: 30px 0;
+    padding: 3px 0;
+    background: var(--grey-background);
+    border-radius: 2em;
+    border: none; 
+    width: 2.4rem;
+    text-align: center;
+    color: var(--grey-lighter);
+    font-size: 0.7em;
+    font-weight: var(--bold);
+    cursor: pointer;
+    
+    :hover{
+        background: var(--green-mint);
+        color: var(--white);
+
+    }
+`
