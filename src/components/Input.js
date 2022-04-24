e84b38a5210f4a8ee76f3dd01fa6670e5cce65a1
zellytozelly <zellytozelly@gmail.com>
2022-04-24 18:52:05 +0900

    feat: add Input step2 email

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,28 +2,52 @@ import React, { useState } from "react";
 import styled from 'styled-components';
 import {ReactComponent as EyeOpenIcon} from '../image/eye-open.svg';
 import {ReactComponent as EyeHideIcon} from '../image/eye-hide.svg';
+import {ReactComponent as CheckIcon} from '../image/circle-check.svg';
 
 export default function Input(){
     
     const [pwd, setPwd] = useState('');
     const [isRevealPwd, setIsRevealPwd] = useState(false);
-    
-
+    const [isEmail, setIsEmail] = useState(false);
+    const checkEmail = (e) => {
+        var emailRegExp = /^[\w-]+(\.[\w-]+)*@([a-z0-9-]+(\.[a-z0-9-]+)*?\.[a-z]{2,6}|(\d{1,3}\.){3}\d{1,3})(:\d{4})?$/;
+        if (emailRegExp.test(e.target.value)) setIsEmail(true); else setIsEmail(false);
+        if (e.target.value == '') console.log("아");
+    }
 
     return (
         <LoginWrapper>
-            <Label>E-mail</Label>
-            <InputText type='email' id='email' label='E-mail' placeholder='E-mail'/> 
 
-            <Label>Password </Label>
+            <EmailBox>
+                <Label>E-mail</Label>
+                <InputText 
+                    type='email'
+                    id='email' 
+                    label='E-mail' 
+                    placeholder='E-mail'
+                    onChange={checkEmail}
+                />
+                <CheckToggle isEmail={isEmail}><CheckIcon/></CheckToggle>
+                <EmailValidText isEmail={isEmail}>Invalid e-mail address.</EmailValidText>
+                
+            </EmailBox>
+            
             <PasswordBox>
-                <InputText type={isRevealPwd ? 'text' : 'password'} value={pwd} onChange={(e) => setPwd(e.target.value)} id='password' label='Password' placeholder='Password'/>    
+                <Label>Password </Label>
+                <InputText 
+                    type={isRevealPwd ? 'text' : 'password'} 
+                    value={pwd} 
+                    onChange={(e) => setPwd(e.target.value)} 
+                    id='password' 
+                    label='Password' 
+                    placeholder='Password'/>    
                 <EyeToggle
                     isRevealPwd = {isRevealPwd}
                     onClick={() => setIsRevealPwd(prevState => !prevState)}
                 >{isRevealPwd ? <EyeOpenIcon/> : <EyeHideIcon/>}
                 </EyeToggle>
-            </PasswordBox>      
+            </PasswordBox> 
+
         </LoginWrapper>
         
 
@@ -33,6 +57,11 @@ export default function Input(){
 const LoginWrapper = styled.div`
     margin: 0 20% 40px 15%;
     padding: 20px;
+`
+const Label = styled.div`
+    margin: 5px;
+    font-size: 0.8em;
+    color: var(--grey);
 `
 const InputText = styled.input`
     width: 100%;
@@ -44,26 +73,44 @@ const InputText = styled.input`
     background: var(--white-second);
     opacity: 0.8;
     box-sizing: border-box
+    font-weight: var(--bold-900);
 
     &:focus{
         border: solid 1px var(--grey);
         opacity: 1;
     }
 `
-
-const Label = styled.div`
+const EmailBox = styled.div`
+    position: relative;
+`
+const EmailValidText = styled.div`
+    font-size: 0.7em;
+    color: var(--red);
+    margin-left: 5px;
+    content: 'Invalid e-mail address.';
+    visibility:  ${(props)=>(props.isEmail) ? 'hidden' : 'visible'};
+`
+const CheckToggle = styled.div`
     margin: 5px;
+    position: absolute;
+    top: 38px;
+    left: 100%;
     font-size: 0.8em;
+    width: 18px;
     color: var(--grey);
+    path{
+        fill: ${(props)=>(props.isEmail) ? "var(--green-mint)" : "var(--grey-lighter)"};
+    }
 `
 const PasswordBox = styled.div`
     position: relative;
+    margin-top: 20px;
 `
 const EyeToggle = styled.div`
     cursor: pointer;
     margin: 5px;
     position: absolute;
-    top: 15px;
+    top: 38px;
     left: calc(100% - 5px);
     font-size: 0.8em;
     width: ${(props)=>(props.isRevealPwd) ? "23px" : "25px"};
